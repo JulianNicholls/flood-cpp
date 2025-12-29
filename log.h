c09217fb3d0e3710261ec77a5305311569c8bd35
Julian Nicholls <juliannicholls29@gmail.com>
2025-12-29 20:41:27 +0000

    Add formatter and logging from ufps.

---
+++ log.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <exception>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <print>
+#include <source_location>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "config.h"
+
+namespace Flood::log
+{
+
+inline std::vector<std::string> history{};
+
+namespace impl
+{
+
+inline static auto log_file = []
+{
+    auto f = std::ofstream{"log", std::ios::app};
+    if (!f)
+    {
+        std::terminate();
+    }
+
+    return f;
+}();
+
+}
+
+enum class Level
+{
+    DEBUG,
+    INFO,
+    WARN,
+    ERR
+};
+
+inline auto g_force_log = true;
+
+template <Level L, class... Args>
+struct Print
+{
+    Print(std::format_string<Args...> msg, Args &&...args, std::source_location loc = std::source_location::current())
+    {
+        if (!(g_force_log))
+        {
+            return;
+        }
+
+        auto c = '?';
+        if constexpr (L == Level::DEBUG)
+        {
+            c = 'D';
+        }
+        else if constexpr (L == Level::INFO)
+        {
+            c = 'I';
+        }
+        else if constexpr (L == Level::WARN)
+        {
+            c = 'W';
+        }
+        else if constexpr (L == Level::ERR)
+        {
+            c = 'E';
+        }
+
+        const auto path = std::filesystem::path{loc.file_name()};
+
+        auto log_line = std::format(
+            "[{}] {}:{} {}", c, path.filename().string(), loc.line(), std::format(msg, std::forward<Args>(args)...));
+
+        std::println("{}", log_line);
+    }
+};
+
+template <Level L = {}, class... Args>
+Print(std::format_string<Args...>, Args &&...) -> Print<L, Args...>;
+
+template <class... Args>
+using debug = Print<Level::DEBUG, Args...>;
+
+template <class... Args>
+using info = Print<Level::INFO, Args...>;
+
+template <class... Args>
+using warn = Print<Level::WARN, Args...>;
+
+template <class... Args>
+using error = Print<Level::ERR, Args...>;
+
+}
